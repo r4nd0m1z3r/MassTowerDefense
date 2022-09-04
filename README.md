2e8217a7b78522adce8d231315b02a4c6231dff2
Dmitry Zinin <dimazin2004@gmail.com>
2022-09-04 14:51:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MassTowerDefense
+
+Developed with Unreal Engine 5
+Simple project to test new Mass system
---
+++ Source/MassTowerDefense/MassTowerDefenseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MassTowerDefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASSTOWERDEFENSE_API AMassTowerDefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
